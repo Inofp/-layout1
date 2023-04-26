d1ab6715d98883053a460f8311ca8ea9453163dd
arimafp <arimafp@mail.ru>
2023-04-26 14:00:48 +0300

    add img

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import { Modal } from './js/Modal';
+
+
+
+window.onload = function() {
+
+    // Render Articles
+    if(data) {
+        renderArticlesToDom();
+    }
+
+    // Tags
+    addTagsClickHandler();
+
+    // Generate Base Modal from Modal Class
+}
+
+const addTagsClickHandler = () => {
+    document.querySelector('.strategies__tags').addEventListener('click', (e) => {
+        if (e.target.classList.contains('tag')) {
+            let clickedTag = e.target;
+            removeSelectedTags();
+            selectClickedTag(clickedTag);
+            if (clickedTag.innerText === 'All') {
+                showAllStrategies();
+            } else {
+                filterStrategyBySelectedTag(clickedTag.innerText);
+            }
+        }
+    })
+}
+
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    })
+}
+
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
+}
+
+const showAllStrategies = () => {
+    let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+    strategies.forEach(strategy => {
+        strategy.classList.remove('strategy_hidden');
+    })
+}
+
+const filterStrategyBySelectedTag = (selectedTag) => {
+    let strategies = document.querySelectorAll('.strategy-wrapper .strategy');
+    strategies.forEach(strategy => {
+        strategy.classList.add('strategy_hidden');
+        strategy.querySelectorAll('.tag').forEach(tag => {
+            if (tag.innerText === selectedTag) {
+                strategy.classList.remove('strategy_hidden');
+            }
+        })
+    })
+
+
+
+const addToolsClickHandler = () => {
+    document.querySelector('.tools__button .button').addEventListener('click', () => {
+        generateToolsModal();
+    })
+}
+
+const generateToolsModal = () => {
+    renderModalWindow('Test content for Tools Modal');
+}
+
+const renderModalWindow = (content) => {
+    let modal =  new Modal ('tools-modal');
+    modal.buildModal(content);
+}
+
